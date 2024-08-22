fcc6a367ed6257b808537a4f9fa48fe1dc4db4d6
A7med-sharaf <a9986939@gmail.com>
2024-08-22 07:28:56 +0300

    update to github

---
+++ index.js
@@ -0,0 +1,29 @@
+let secondumber;
+let firstnumber;
+function clac(){
+     firstnumber=+prompt("أدخل الرقم الاول")
+     secondumber=+prompt("أدخل الرقم الثاني")
+
+}
+
+
+function add(){
+   let sum = firstnumber + secondumber
+    console.log(sum);
+    
+}
+function sub(){
+    let sum = firstnumber - secondumber
+     console.log(sum);
+     
+ }
+ function div(){
+    let sum = firstnumber / secondumber
+     console.log(sum);
+     
+ }
+ function mlut(){
+    let sum = firstnumber + secondumber
+     console.log(sum);
+     
+ }
